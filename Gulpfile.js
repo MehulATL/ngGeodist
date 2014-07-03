2df272f683bb91a79092304aa2a737d563488711
Mehul Patel <mehul8@gmail.com>
2014-07-02 23:43:08 -0700

    add Gulpfile.js

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+var gulp    = require('gulp'),
+    concat  = require('gulp-concat');
+
+var paths = {
+  src: [
+    './src/index.js',
+    './src/services/service.js',
+  ],
+  dist: './dist/'
+};
+
+gulp.task('concat', function(){
+  return gulp.src(paths.src)
+    .pipe(concat('ng-geodist.js'))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('build', ['concat']);
+
+gulp.task('watch', function(){
+  gulp.watch(paths.src, ['build']);
+});
+
+gulp.task('default', ['build' ,'watch']);
