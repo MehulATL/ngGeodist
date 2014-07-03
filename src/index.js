04fecb0a92749c08af4a899a0cd434aecf42723c
Mehul Patel <mehul8@gmail.com>
2014-07-03 00:14:16 -0700

    rename vars

--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
-angular.module('ngGeodist', [
-  ngGeodist.service
-]);
+angular.module('ngGeodist', [])
+
+.factory('ngGeodist', function() {
+
+  var radiusUnits = {
+    'feet': 20908800,
+    'yards': 6969600,
+    'miles': 3960,
+    'mi': 3960,
+    'kilometers': 6371,
+    'km': 6371,
+    'meters': 6371000
+  };
+
+  var parseCoordinates = function(point) {
+    if (point === null) {
+      point = [0, 0];
+    }
+    var coords = [];
+    if (Array.isArray(point)) {
+      coords = point;
+    } else if ((point.lat !== null) && (point.lng !== null)) {
+      coords = [point.lat, point.lng];
+    } else if (typeof point === 'object') {
+      for (var key in point) {
+        var val = point[key];
+        coords.push(val);
+      }
+    } else {
+      coords = point;
+    }
+    return coords;
+  };
+
+  var getEarthRadius = function(unit) {
+    if (unit === null) {
+      unit = "miles";
+    }
+    unit = unit.toLowerCase();
+    if (!radiusUnits[unit]) {
+      unit = "miles";
+    }
+    return radiusUnits[unit];
+  };
+
+  return {
+
+    getDistance: function(start, end, options) {
+
+      if (options === null) {
+        options = {};
+      }
+
+      var _ref = parseCoordinates(start);
+      var lat1 = _ref[0];
+      var lng1 = _ref[1];
+      var _ref1 = parseCoordinates(end);
+      var lat2 = _ref1[0];
+      var lng2 = _ref1[1];
+      var earthRadius = getEarthRadius('options.unit');
+      var latDelta = (lat2 - lat1) * Math.PI / 180;
+      var lngDelta = (lng2 - lng1) * Math.PI / 180;
+      var lat1Rad = lat1 * Math.PI / 180;
+      var lat2Rad = lat2 * Math.PI / 180;
+      var a = Math.sin(latDelta / 2) * Math.sin(latDelta / 2) + Math.sin(lngDelta / 2) * Math.sin(lngDelta / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var distance = earthRadius * c;
+
+      if (!options.exact) {
+        distance = Math.floor(distance);
+      }
+
+      if (options.limit) {
+        if (options.limit > distance) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      if (options.format) {
+        distance = "" + distance + " " + (options.unit || 'miles');
+      }
+
+      return distance;
+
+    }
+
+  };
+
+});
